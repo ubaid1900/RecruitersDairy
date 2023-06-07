46554785ee1b3199ac7af7bd3dcc71e38f90f2c4
ubaid1900 <ubaid1900@gmail.com>
2023-06-07 09:42:39 +0530

    seed jobs data

--- frontend/data.js
+++ frontend/data.js
@@ -1,6 +1,250 @@
-function seedData()
- {
-   var dataString = '[{"createdDate":"2023-05-26T16:53:46.472Z","updatedDate":"2023-05-26T16:53:46.472Z","jobid":"1","description":"software developer","candidates":[{"name":"Jhon","address":"Bangalore","phone":"908779078","email":"john@hotmail.com","notes":"third round compleated"},{"name":"Mohd saleem","address":"Banjara Hills Hyderabad","phone":"06305397499","email":"saleem@gmail.com","notes":"Selected"},{"name":"Ramchandra","address":"Chanda Nagar","phone":"98342132","email":"ram@gmail.com","notes":"waiting for Hr round"}]},{"jobid":"2","description":"Tech support","candidates":[{"name":"Russel","address":"Bangalore","phone":"568999910","email":"russ@gmail.com","notes":"negotiation in HR round"},{"name":"Chandra Shaker","address":"ORR Hyderabad","phone":"987663663","email":"shaker@hotmail.com","notes":"waiting for offer letter"},{"name":"Abdul Rahman","address":"Akber bagh Hyderabad","phone":"98457383","email":"abdul@yahoo.com","notes":"selected"},{"name":"kaleem","address":"Wahed Nager, Hyderabad ","phone":"87634343","email":"kal@gamil.com","notes":"HR round schdulded"},{"name":"Sri laxmi","address":"Ramu Nagar Hyderabad","phone":"98677381","email":"laxmi@yahoo.com","notes":"Fresher first round completed."},{"name":"Samuel","address":"Banglore","phone":"78883354","email":"sam@gmail.com","notes":"three rounds completed"}]},{"jobid":"3","description":"Networking","candidates":[{"name":"Srichaitanya","address":"Dilsukh Nagar","phone":"98747774","email":"sri@gmail.com","notes":"waiting for HR round"},{"name":"Abdul shah","address":"Amberpet Hyderabad","phone":"98474774","email":"shah@yahoo.com","notes":"waiting for response"},{"name":"Ramulu","address":"Chanda Nagar Hyderabad","phone":"987878343","email":"ram@hotmail.com","notes":"waiting for offer letter"}]},{"jobid":"4","description":"Finance","candidates":[{"name":"Ibrahim","address":"Mothi Nagar","phone":"89334433","email":"shan@yahoo.com","notes":"waiting of offer letter"},{"name":"Abdul Asad","address":"Banglore","phone":"23423432","email":"asad@yahoo.com","notes":"Appointed"}]},{"createdDate":"2023-05-25T17:31:40.953Z","updatedDate":"2023-05-25T17:31:40.953Z","jobid":"5","description":"Front End","candidates":[{"name":"Mohd Arif","address":"Banglore","phone":"23423453","email":"arif@hotmai.com","notes":"waiting of HR round"},{"name":"Sahik Mohammed","address":"Lic colony","phone":"234523423","email":"mohammed@gamil.com","notes":"three rounds compleated"},{"name":"Salman","address":"Amberpet","phone":"2324123","email":"Salman@gmail.com","notes":"waiting for ineterview"}]},{"jobid":"6","description":"Administration","candidates":[{"name":"Swamy","address":"Delhi","phone":"2432343","email":"sham@gmail.com","notes":"Appointed"}]}]';
-   localStorage.setItem("jobs1","dataString")
+function setSeedData() {
+  var seedData = [
+    {
+      "jobid": "1",
+      "description": "software developer",
+      "client": 'TCS',
+      "requirements": '5 years of .NET, 5 years of javascript',
+      "jobLocation": 'Hyderabad',
+      "jobPayRange": '10L-20L',
+      "payPeriod": "Yearly",
+      "createdDate": new Date(),
+      "updatedDate": new Date(),
+      "candidates": [{
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Jhon",
+        "address": "Bangalore",
+        "phone": "908779078",
+        "email": "john@hotmail.com",
+        "submissionStatus": 'Grooming',
+        "notes": "third round completed"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Mohd saleem",
+        "address": "Banjara Hills Hyderabad",
+        "phone": "06305397499",
+        "email": "saleem@gmail.com",
+        "submissionStatus": 'Grooming',
+        "notes": "Selected"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Ramchandra",
+        "address": "Chanda Nagar",
+        "phone": "98342132",
+        "email": "ram@gmail.com",
+        "submissionStatus": 'Grooming',
+        "notes": "waiting for Hr round"
+      }]
+    },
+    {
+      "jobid": "2",
+      "description": "Tech support",
+      "client": 'TCS',
+      "requirements": '3 years of experience in a similar role',
+      "jobLocation": 'Hyderabad',
+      "jobPayRange": '10K-20K',
+      "payPeriod": "Monthly",
+      "createdDate": new Date(),
+      "updatedDate": new Date(),
+      "candidates": [{
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Russel",
+        "address": "Bangalore",
+        "phone": "568999910",
+        "email": "russ@gmail.com",
+        "submissionStatus": 'Grooming',
+        "notes": "negotiation in HR round"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Chandra Shaker",
+        "address": "ORR Hyderabad",
+        "phone": "987663663",
+        "email": "shaker@hotmail.com",
+        "submissionStatus": 'Grooming',
+        "notes": "waiting for offer letter"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Abdul Rahman",
+        "address": "Akber bagh Hyderabad",
+        "phone": "98457383",
+        "email": "abdul@yahoo.com",
+        "submissionStatus": 'Grooming',
+        "notes": "selected"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "kaleem",
+        "address": "Wahed Nager, Hyderabad ",
+        "phone": "87634343",
+        "email": "kal@gamil.com",
+        "submissionStatus": 'Grooming',
+        "notes": "HR round schdulded"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Sri laxmi",
+        "address": "Ramu Nagar Hyderabad",
+        "phone": "98677381",
+        "email": "laxmi@yahoo.com",
+        "submissionStatus": 'Grooming',
+        "notes": "Fresher first round completed."
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Samuel",
+        "address": "Banglore",
+        "phone": "78883354",
+        "email": "sam@gmail.com",
+        "submissionStatus": 'Grooming',
+        "notes": "three rounds completed"
+      }]
+    },
+    {
+      "jobid": "3",
+      "description": "Networking",
+      "client": 'Infosys',
+      "requirements": '3 years of experience in a similar role',
+      "jobLocation": 'Hyderabad',
+      "jobPayRange": '10K-20K',
+      "payPeriod": "Monthly",
+      "createdDate": new Date(),
+      "updatedDate": new Date(),
+      "candidates": [{
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Srichaitanya",
+        "address": "Dilsukh Nagar",
+        "phone": "98747774",
+        "email": "sri@gmail.com",
+        "submissionStatus": 'Scheduled for interview',
+        "notes": "waiting for HR round"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Abdul shah",
+        "address": "Amberpet Hyderabad",
+        "phone": "98474774",
+        "email": "shah@yahoo.com",
+        "submissionStatus": 'Interviewed',
+        "notes": "waiting for response"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Ramulu",
+        "address": "Chanda Nagar Hyderabad",
+        "phone": "987878343",
+        "email": "ram@hotmail.com",
+        "submissionStatus": 'Grooming',
+        "notes": "waiting for offer letter"
+      }]
+    },
+    {
+      "jobid": "4",
+      "description": "Finance",
+      "client": 'TCS',
+      "requirements": '3 years of experience in a similar role',
+      "jobLocation": 'Hyderabad',
+      "jobPayRange": '10K-20K',
+      "payPeriod": "Monthly",
+      "createdDate": new Date(),
+      "updatedDate": new Date(),
+      "candidates": [{
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Ibrahim",
+        "address": "Mothi Nagar",
+        "phone": "89334433",
+        "email": "shan@yahoo.com",
+        "submissionStatus": 'Grooming',
+        "notes": "waiting of offer letter"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Abdul Asad",
+        "address": "Banglore",
+        "phone": "23423432",
+        "email": "asad@yahoo.com",
+        "submissionStatus": 'Grooming',
+        "notes": "Appointed"
+      }]
+    },
+    {
+      "jobid": "5",
+      "description": "Front End",
+      "client": 'TCS',
+      "requirements": '3 years of experience in a similar role',
+      "jobLocation": 'Hyderabad',
+      "jobPayRange": '10K-20K',
+      "payPeriod": "Monthly",
+      "createdDate": new Date(),
+      "updatedDate": new Date(),
+      "candidates": [{
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Mohd Arif",
+        "address": "Banglore",
+        "phone": "23423453",
+        "email": "arif@hotmai.com",
+        "submissionStatus": 'Grooming',
+        "notes": "waiting of HR round"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Sahik Mohammed",
+        "address": "Lic colony",
+        "phone": "234523423",
+        "email": "mohammed@gamil.com",
+        "submissionStatus": 'Grooming',
+        "notes": "three rounds completed"
+      },
+      {
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Salman",
+        "address": "Amberpet",
+        "phone": "2324123",
+        "email": "Salman@gmail.com",
+        "submissionStatus": 'Grooming',
+        "notes": "waiting for ineterview"
+      }]
+    },
+    {
+      "jobid": "6",
+      "description": "Administration",
+      "client": 'TCS',
+      "requirements": '3 years of experience in a similar role',
+      "jobLocation": 'Hyderabad',
+      "jobPayRange": '10K-20K',
+      "payPeriod": "Monthly",
+      "createdDate": new Date(),
+      "updatedDate": new Date(),
+      "candidates": [{
+        "createdDate": new Date(),
+        "updatedDate": new Date(),
+        "name": "Swamy",
+        "address": "Delhi",
+        "phone": "2432343",
+        "email": "sham@gmail.com",
+        "notes": "Appointed"
+      }]
+    }];
+   localStorage.setItem("jobs", JSON.stringify(seedData))
 }
 
