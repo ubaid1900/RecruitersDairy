cace0a0c306fe7125fa6938af99ccae7d09aa041
safi_mohd_2000 <safi_mohd_2000@yahoo.com>
2023-06-15 03:46:11 -0700

    payperiod

---
+++ frontend/uiHelper1.js
@@ -0,0 +1,34 @@
+function getPayPeriod() {
+    var divpay = document.getElementById("payPeriod");
+    var payPeriodLbl = document.createElement("label");
+    payPeriodLbl.innerText = "Pay Period";
+    const selectElement = document.createElement('select');
+    selectElement.setAttribute("id", "payPeriodId");
+    selectElement.setAttribute('title', 'Please select a PayType');
+
+    divpay.appendChild(payPeriodLbl);
+    divpay.appendChild(selectElement);
+    
+    const options = getPayPeriodOptions();
+    for (let index = 0; index < options.length; index++) {
+        const element = options[index];
+        const optionElement = document.createElement('option');
+        optionElement.value = element.id; optionElement.text = element.text;
+            optionElement.selected = true;
+        selectElement.append(optionElement);
+    }
+    return selectElement;
+   
+}
+
+function getPayPeriodOptions() {
+    return [
+        {
+            id: 1, text: 'Monthly'
+        },
+        {
+            id: 2, text: 'Yearly'
+        }
+
+    ];
+}
